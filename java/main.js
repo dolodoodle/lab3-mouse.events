b67dda211ec088135b2cd47bafe584034252fa66
dolodoodle <dltokidoki@gmail.com>
2022-02-26 14:34:44 -0800

    added "where did i click?"

--- java/main.js
+++ java/main.js
@@ -4,3 +4,18 @@ document.body.addEventListener('click', (e) => {
     dot.style.transform += `translateX(${e.clientX - 25}px)`;
     dot.style.opacity = 1;
 }, false);
+
+//create shortcut constants
+const demo  = document.querySelector('.demo');
+const point = document.querySelector('#point');
+
+//detect mousedown events and handle them.
+demo.addEventListener('mousedown', (e) => {
+    point.innerHTML = e.offsetX + ', ' + e.offsetY;
+    
+    //open the JS console to see the output
+    console.log("Client: " , e.clientX , e.clientY)
+    console.log("Page: " , e.pageX , e.pageY)
+    console.log("Screen: " , e.screenX , e.screenY)
+    console.log("Event Object: ", e);
+});
